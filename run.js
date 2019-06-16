670e14eec886edc05c12dc5eefe20a68d8a9e990
Spyromain <spyromain@gmail.com>
2019-06-16 19:17:06 +0200

    Màj anti-fiano : ajout détection ok dans les images + Refactorisation fonctionnelle du code

--- run.js
+++ run.js
@@ -7,34 +7,78 @@ const FIANO_ID = "307984283774222348"
 const Discord = require("discord.js")
 const client = new Discord.Client()
 
-function removeDiacritics(string) {
-    return string.normalize('NFD').replace(/[\u0300-\u036f]/g, "")
-}
+const Tesseract = require("tesseract.js")
+const worker = new Tesseract.TesseractWorker()
 
-client.on("ready", () => {
-    console.log(`Logged in as ${client.user.tag}`)
-})
+const removeDiacritics = (string) =>
+    string.normalize('NFD').replace(/[\u0300-\u036f]/g, "")
 
-client.on("message", (message) => {
-    if (message.content.startsWith("!ping")) {
-        message.reply("pong !")
-    }
-    else if (message.content.startsWith("!start-server")) {
-        exec("../start_public.sh", (error, stdout, stderr) => {
-            if (error) {
-                message.channel.send("Erreur : " + error.message)
-                return
-            }
-            message.channel.send(stdout)
-        })
-    }
-
-    if (message.author.id === FIANO_ID && removeDiacritics(message.content).toUpperCase().includes("OK")) {
-        message.channel.send(`🌩️ Que la foudre s'abatte sur ${message.author} ! 🌩️`)
-        .then(() => {
+const antiFiano = (message) =>
+    message.author.id === FIANO_ID
+    ? removeDiacritics(message.content).toUpperCase().includes("OK")
+        ? message.channel.send(`🌩️ Que la foudre s'abatte sur ${message.author} ! 🌩️`)
+        .then(() =>
             message.delete()
-        })
-    }
-})
+        )
+        : message.attachments
+        .filter((attachment) => attachment.width != null)
+        .map((attachment) => attachment.url)
+        .concat(
+            message.embeds
+            .filter((embed) => embed.thumbnail != null)
+            .map((embed) => embed.thumbnail.url)
+        )
+        .reduce((accumulator, value) =>
+            accumulator
+            .then((ok) =>
+                ok
+                ? Promise.resolve(true)
+                : new Promise((resolve, reject) =>
+                    worker
+                    .recognize(value)
+                    .then((result) =>
+                        resolve(removeDiacritics(result.text).toUpperCase().includes("OK"))
+                    )
+                )
+            )
+        , Promise.resolve(false))
+        .then((ok) =>
+            ok
+            ? message.channel
+            .send(`🌩️🌩️🌩️ Que la foudre s'abatte **violemment** sur ${message.author} !!! 🌩️🌩️🌩️`)
+            .then(() =>
+                message.delete()
+            )
+            : Promise.resolve()
+        )
+        : Promise.resolve()
+
+client.on("ready", () =>
+    console.log(`Logged in as ${client.user.tag}`)
+)
+
+client.on("message", (message) =>
+    message.content.startsWith("!ping")
+        ? message.reply("pong !")
+        : message.content.startsWith("!start-server")
+            ? new Promise((resolve) =>
+                exec("../start_public.sh", resolve)
+            )
+            .then((error, stdout, stderr) =>
+                error
+                ? message.channel.send("Erreur : " + error.message)
+                : message.channel.send(stdout)
+            )
+            : Promise.resolve()
+    .then(() =>
+        antiFiano(message)
+    )
+)
+
+client.on("messageUpdate", (message) =>
+    message.author.id === FIANO_ID
+    ? antiFiano(message)
+    : Promise.resolve()
+)
 
 client.login(CHAMPIA_TOKEN)
