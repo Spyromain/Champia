ffe4373cab9665531d1204889887437d9efd71fa
Spyromain <spyromain@gmail.com>
2019-06-18 22:01:26 +0200

    Màj Promise -> async/await

--- run.js
+++ run.js
@@ -13,78 +13,75 @@ const client = new Discord.Client()
 const Tesseract = require("tesseract.js")
 const worker = new Tesseract.TesseractWorker()
 
-const removeDiacritics = (string) =>
-    string.normalize('NFD').replace(/[\u0300-\u036f]/g, "")
+const removeDiacritics = (string) => {
+    return string.normalize('NFD').replace(/[\u0300-\u036f]/g, "")
+}
 
-const antiFiano = (message) =>
-    message.author.id === FIANO_ID && message.channel.id !== CHANNEL_FIANOWORLD_ID
-    ? removeDiacritics(message.content.replace(/\s/g, "")).toUpperCase().includes("OK")
-        ? message.channel.send(`🌩️ Que la foudre s'abatte sur ${message.author} ! 🌩️`)
-        .then(() =>
-            message.delete()
-        )
-        : message.attachments
-        .filter((attachment) => attachment.width != null)
-        .map((attachment) => attachment.url)
-        .concat(
-            message.embeds
-            .filter((embed) => embed.thumbnail != null)
-            .map((embed) => embed.thumbnail.url)
-        )
-        .reduce((accumulator, value) =>
-            accumulator
-            .then((ok) =>
-                ok
-                ? Promise.resolve(true)
-                : new Promise((resolve, reject) =>
-                    worker
-                    .recognize(value)
-                    .then((result) =>
-                        client.channels
-                        .get(CHANNEL_CONFIG_BOT_ID)
-                        .send("[Tesseract] " + result.text)
-                        .then(() =>
+const antiFiano = async (message) => {
+    if (message.author.id === FIANO_ID && message.channel.id !== CHANNEL_FIANOWORLD_ID) {
+        if (removeDiacritics(message.content.replace(/\s/g, "")).toUpperCase().includes("OK")) {
+            await message.channel.send(`🌩️ Que la foudre s'abatte sur ${message.author} ! 🌩️`)
+            await message.delete()
+        }
+        else {
+            ok = await message.attachments
+            .filter((attachment) => attachment.width != null)
+            .map((attachment) => attachment.url)
+            .concat(
+                message.embeds
+                .filter((embed) => embed.thumbnail != null)
+                .map((embed) => embed.thumbnail.url)
+            )
+            .reduce(async (accumulator, value) => {
+                if (await accumulator) {
+                    return true
+                }
+                else {
+                    return await new Promise((resolve, reject) =>
+                        worker
+                        .recognize(value)
+                        .then((result) => {
+                            client.channels
+                            .get(CHANNEL_CONFIG_BOT_ID)
+                            .send("[Tesseract] " + result.text)
+
                             resolve(removeDiacritics(result.text).toUpperCase().includes("OK"))
-                        )
+                        })
                     )
-                )
-            )
-        , Promise.resolve(false))
-        .then((ok) =>
-            ok
-            ? message.channel
-            .send(`🌩️🌩️🌩️ Que la foudre s'abatte **violemment** sur ${message.author} !!! 🌩️🌩️🌩️`)
-            .then(() =>
-                message.delete()
-            )
-            : Promise.resolve()
-        )
-        : Promise.resolve()
+                }
+            }, Promise.resolve(false))
 
-client.on("ready", () =>
+            if (ok) {
+                await message.channel
+                .send(`🌩️🌩️🌩️ Que la foudre s'abatte **violemment** sur ${message.author} !!! 🌩️🌩️🌩️`)
+
+                await message.delete()
+            }
+        }
+    }
+}
+
+client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}`)
-)
+})
 
-client.on("message", (message) =>
-    message.content.startsWith("!ping")
-        ? message.reply("pong !")
-        : message.content.startsWith("!start-server")
-            ? new Promise((resolve) =>
-                exec("../start_public.sh", (error, stdout, stderr) =>
-                    resolve(stdout)
-                )
+client.on("message", async (message) => {
+    if (message.content.startsWith("!ping")) {
+        await message.reply("pong !")
+    }
+    else if (message.content.startsWith("!start-server")) {
+        output = await new Promise((resolve) => {
+            exec("../start_public.sh", (error, stdout, stderr) =>
+                resolve(stdout)
             )
-            .then((stdout) =>
-                message.channel.send(stdout || "Une erreur est survenue")
-            )
-            : Promise.resolve()
-    .then(() =>
-        antiFiano(message)
-    )
-)
+        })
+        await message.channel.send(output || "Une erreur est survenue")
+    }
+    await antiFiano(message)
+})
 
-client.on("messageUpdate", (message) =>
-    antiFiano(message)
-)
+client.on("messageUpdate", async (message) => {
+    await antiFiano(message)
+})
 
 client.login(CHAMPIA_TOKEN)
